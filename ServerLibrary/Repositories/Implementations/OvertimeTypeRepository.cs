78f56c74195706ab8931ac5e568d9832a84cb930
ChethanaH06 <114866980+ChethanaH06@users.noreply.github.com>
2024-10-17 12:30:31 +0530

    Server library commit

---
+++ ServerLibrary/Repositories/Implementations/OvertimeTypeRepository.cs
@@ -0,0 +1,52 @@
+﻿using BaseLibrary.Entities;
+using BaseLibrary.Responses;
+using Microsoft.EntityFrameworkCore;
+using ServerLibrary.Data;
+using ServerLibrary.Repositories.Contracts;
+
+namespace ServerLibrary.Repositories.Implementations
+{
+    public class OvertimeTypeRepository(AppDbContext appDbContext):IGenericRepositoryInterface<OvertimeType>
+    {
+        public async Task<GeneralResponse> DeleteById(int id)
+        {
+            var item = await appDbContext.OvertimesTypes.FindAsync(id);
+            if (item is null) return NotFound();
+
+            appDbContext.OvertimesTypes.Remove(item);
+            await Commit();
+            return Success();
+        }
+
+        public async Task<List<OvertimeType>> GetAll() => await appDbContext.OvertimesTypes.AsNoTracking().ToListAsync();
+
+        public async Task<OvertimeType> GetById(int id) => await appDbContext.OvertimesTypes.FindAsync(id);
+
+        public async Task<GeneralResponse> Insert(OvertimeType item)
+        {
+            if (!await CheckName(item.Name!))
+                return new GeneralResponse(false, "Overtime type already added");
+            appDbContext.OvertimesTypes.Add(item);
+            await Commit();
+            return Success();
+        }
+
+        public async Task<GeneralResponse> Update(OvertimeType item)
+        {
+            var obj = await appDbContext.OvertimesTypes.FindAsync(item.Id);
+            if (obj is null) return NotFound();
+            obj.Name = item.Name;
+            await Commit();
+            return Success();
+        }
+
+        private static GeneralResponse NotFound() => new(false, "Sorry data not found");
+        private static GeneralResponse Success() => new(true, "Process completed");
+        private async Task Commit() => await appDbContext.SaveChangesAsync();
+        private async Task<bool> CheckName(string name)
+        {
+            var item = await appDbContext.OvertimesTypes.FirstOrDefaultAsync(x => x.Name!.ToLower().Equals(name.ToLower()));
+            return item is null;
+        }
+    }
+}
---
+++ ServerLibrary/Repositories/Implementations/SanctionRepository.cs
@@ -0,0 +1,54 @@
+﻿using BaseLibrary.Entities;
+using BaseLibrary.Responses;
+using Microsoft.EntityFrameworkCore;
+using ServerLibrary.Data;
+using ServerLibrary.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerLibrary.Repositories.Implementations
+{
+    public class SanctionRepository(AppDbContext appDbContext):IGenericRepositoryInterface<Sanction>
+    {
+        public async Task<GeneralResponse> DeleteById(int id)
+        {
+            var item = await appDbContext.Sanctions.FirstOrDefaultAsync(x => x.EmployeeId == id);
+            if (item is null) return NotFound();
+
+            appDbContext.Sanctions.Remove(item);
+            await Commit();
+            return Success();
+        }
+
+        public async Task<List<Sanction>> GetAll() => await appDbContext.Sanctions.AsNoTracking().Include(o=>o.SanctionType).ToListAsync();
+
+        public async Task<Sanction> GetById(int id) => await appDbContext.Sanctions.FirstOrDefaultAsync(c => c.EmployeeId == id);
+
+        public async Task<GeneralResponse> Insert(Sanction item)
+        {
+            appDbContext.Sanctions.Add(item);
+            await Commit();
+            return Success();
+        }
+
+        public async Task<GeneralResponse> Update(Sanction item)
+        {
+            var obj = await appDbContext.Sanctions.FirstOrDefaultAsync(n => n.EmployeeId == item.EmployeeId);
+            if (obj is null) return NotFound();
+            obj.SanctionType = item.SanctionType;
+            obj.PunishmentDate = item.PunishmentDate;
+            obj.Date = item.Date;
+            obj.Punishment= item.Punishment;
+            await Commit();
+            return Success();
+        }
+
+
+        private static GeneralResponse NotFound() => new(false, "Sorry data not found");
+        private static GeneralResponse Success() => new(true, "Process completed");
+        private async Task Commit() => await appDbContext.SaveChangesAsync();
+    }
+}
